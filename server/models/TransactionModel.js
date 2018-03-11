8d6684cd0b51501cffd9dba83840a60ab5e5e8c6
Eric Fileti <github.com@ericfileti.com>
2018-03-11 15:23:12 -0700

    refactored agent with timing class and dependent server & cleint code

--- server/models/TransactionModel.js
+++ server/models/TransactionModel.js
@@ -1,20 +1,29 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const traceSchema = new Schema({
-  'type': String,
-  'uuid': String,
-  'duration': Number
-})
-
-const transactionSchema = new Schema({
-  'requestUrl': String,
-  'requestMethod': String, 
-  'routeName': String,
-  'transactionId': String,
-  'traces': [traceSchema] 
+  route: String,
+  method: String,
+  library: String,
+  type: String,
+  startTimestamp: Date,
+  endTimestamp: Date,
+  duration: Number
 });
 
-const Transaction = mongoose.model('Transaction', transactionSchema);
-const Trace = mongoose.model('Trace', traceSchema);
+const transactionSchema = new Schema({
+  route: String,
+  method: String,
+  userAgent: String,
+  rawHeaders: String,
+  cookies: String,
+  remoteAddress: String,
+  startTimestamp: Date,
+  endTimestamp: Date,
+  duration: Number,
+  traces: [traceSchema]
+});
+
+const Transaction = mongoose.model("Transaction", transactionSchema);
+const Trace = mongoose.model("Trace", traceSchema);
 module.exports = { Transaction, Trace };
